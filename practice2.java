3b20fc84cca7c7de3651ea143c98a44d3c2ea78d
Rohan Tosh <126614478+ToshRohan@users.noreply.github.com>
2023-03-02 00:53:17 +0530

    Update practice2.java

--- practice2.java
+++ practice2.java
@@ -6,10 +6,10 @@ public class practice2 {
         
         
         Scanner me=new Scanner(System.in);
-        System.out.println("Enter your country name");
+        System.out.println("Enter your name");
         name=me.nextLine();
         System.out.println("Enter your age");
-        age=me.nextInt
+        age=me.nextInt();
         System.out.println("My name is "+name);
         System.out.println("\nAge is "+age);
     }
