aa9b90ab9ac0ecc12dc5e280e1b82b4a986a97e1
Rohan Tosh <rohantosh3@gmail.com>
2023-03-02 00:13:30 +0530

    first commit

---
+++ practice2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class practice2 {
+    public static void main(String  args[]){
+        String country;
+        System.out.println("Enter your country name");
+        Scanner me=new Scanner(System.in);
+        country=me.nextLine();
+        System.out.println("Nmae of my country is: "+country);
+    }
+    
+}
